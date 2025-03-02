298e47494e3d30c0995e6d796e732c0f5e1611a1
rnglike <58084632+rnglike@users.noreply.github.com>
2025-03-02 18:11:53 -0300

    Add PlayerController script

---
+++ pombo-jam/Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+    [SerializeField] private float weight = 1.0f;
+
+    private void Start()
+    {
+        controller = AddCharacterControllerSafe(gameObject);
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        // Makes the player jump
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * weight * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    CharacterController AddCharacterControllerSafe(GameObject gameObject)
+    {
+        CharacterController controller = gameObject.GetComponent<CharacterController>();
+        return controller != null ? controller : null;
+    }
+}
